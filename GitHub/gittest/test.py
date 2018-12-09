bc01f628693d5fd4495e54511eec87a798103cf3
alexlisnitskiy <alexlisnitskiy@gmail.com>
2018-12-09 22:36:03 +0200

    adding End of list message

--- GitHub/gittest/test.py
+++ GitHub/gittest/test.py
@@ -6,6 +6,7 @@ print("This program shows list of all files in current directory")
 str = raw_input("Please, set file extention if needed: ")
 if str == "":
     str = "*"
-os.chdir(os.getcwd())
+# os.chdir(os.getcwd())
 for file in glob.glob("*." + str):
     print(file)
+print("End of list")
